e8cd8ceb3f5de0ae405137ee688d5fcf5b4ee2a3
Xiande Wen <visiande1998@gmail.com>
2021-03-09 20:10:44 +1100

    Update object pool class

---
+++ src/scenes/MainScene/utils/Pool.js
@@ -0,0 +1,62 @@
+// Ref: https://blog.ourcade.co/posts/2020/phaser-3-optimization-object-pool-basic/
+
+import Phaser from "../../../libs/phaser-full.min";
+
+class CustomMatterImage extends Phaser.Physics.Matter.Image {
+    constructor(scene, x, y, key, frame, options) {
+        super(scene.matter.world, x, y, key, frame, options);
+        this.setIgnoreGravity(true);
+    }
+}
+
+class Pool extends Phaser.GameObjects.Group {
+    constructor(scene, config) {
+        const defaults = {
+            classType: CustomMatterImage,
+            maxSize: -1
+        }
+
+        super(scene, Object.assign(defaults, config))
+    }
+
+    spawn(x = 0, y = 0, key = this.defaultKey, frame = this.defaultFrame) {
+        const spawnExisting = this.countActive(false) > 0;
+
+        const obj = this.get(x, y, key, frame);
+
+        if (spawnExisting) {
+            obj.setVisible(true)
+            obj.setActive(true)
+            obj.world && obj.world.add(obj.body);
+        }
+
+        return obj;
+    }
+
+    despawn(obj) {
+        this.killAndHide(obj)
+
+        obj.alpha = 1
+        obj.scale = 1
+        obj.removeInteractive()
+        obj.world && obj.world.remove(obj.body)
+    }
+
+    initializeWithSize(size) {
+        if (this.getLength() > 0 || size <= 0) {
+            return
+        }
+
+        this.createMultiple({
+            key: this.defaultKey, 
+            frame: this.defaultFrame,
+            quantity: size,
+            // visible: false,
+            // active: false
+        })
+    }
+}
+
+Phaser.GameObjects.GameObjectFactory.register('pool', function (config) {
+    return this.updateList.add(new Pool(this.scene, config));
+});
